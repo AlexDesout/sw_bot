e144c43f33eda1969d1f76e2d1b2638a3d851440
Alexandre Desoutter <alexd@MacBook-Pro-de-Alexandre.local>
2024-08-24 19:33:37 +0200

    first commit

---
+++ commands/utility/number.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('number')
+		.setDescription('Display a random number.'),
+	async execute(interaction) {
+		await interaction.reply(`Your number ${Math.floor(Math.random() * 1000)}`);
+	},
+};
